38a2b6a23ecff90d896259006212101a9ec65f6e
Zachary Schneider <z@platform.engineering>
2026-04-02 13:16:10 -0400

    remove vals.go

--- internal/cli/clean/clean.go
+++ internal/cli/clean/clean.go
@@ -7,12 +7,9 @@ package clean
 import (
 	"fmt"
 	"log/slog"
-	"os"
-	"path/filepath"
 
-	"github.com/platform-engineering-labs/formae/internal/cli/cmd"
+	clicmd "github.com/platform-engineering-labs/formae/internal/cli/cmd"
 	"github.com/platform-engineering-labs/formae/internal/opsmgr"
-	"github.com/platform-engineering-labs/orbital/mgr"
 	"github.com/spf13/cobra"
 )
 
@@ -25,21 +22,21 @@ func CleanCmd() *cobra.Command {
 			"examples": "{{.Name}} {{.Command}}",
 		},
 		SilenceErrors: true,
-		RunE: func(command *cobra.Command, args []string) error {
-			binPath, err := os.Executable()
-			if err != nil {
-				return fmt.Errorf("could not determine binary path: %w", err)
-			}
-			root := filepath.Dir(filepath.Dir(binPath))
+		RunE: func(cmd *cobra.Command, args []string) error {
+			configFile, _ := cmd.Flags().GetString("config")
 
-			orb, err := mgr.New(slog.Default(), root, opsmgr.TreeConfig)
+			app, err := clicmd.AppFromContext(cmd.Context(), configFile, "", cmd)
 			if err != nil {
 				return err
 			}
 
-			// init root if needed
+			orb, err := opsmgr.New(slog.Default(), app.Config.Artifacts.URL, "")
+			if err != nil {
+				return err
+			}
+
 			if !orb.Ready() {
-				return fmt.Errorf("no managed installation root detected at: %s\n", root)
+				return fmt.Errorf("no managed installation root detected at: %s\n", orb.Path)
 			}
 
 			fmt.Println("done.")
@@ -48,7 +45,8 @@ func CleanCmd() *cobra.Command {
 		},
 	}
 
-	command.SetUsageTemplate(cmd.SimpleCmdUsageTemplate)
+	command.SetUsageTemplate(clicmd.SimpleCmdUsageTemplate)
+	command.Flags().String("config", "", "Path to config file")
 
 	return command
 }
